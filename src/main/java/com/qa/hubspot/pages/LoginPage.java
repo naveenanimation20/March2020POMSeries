69498d9d6a1bf6f7aa7c273848853535ed2ff97a
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2020-06-03 10:50:53 +0530

    added allure report listener

--- src/main/java/com/qa/hubspot/pages/LoginPage.java
+++ src/main/java/com/qa/hubspot/pages/LoginPage.java
@@ -1,12 +1,13 @@
 package com.qa.hubspot.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 
 import com.qa.hubspot.base.BasePage;
 import com.qa.hubspot.utils.Constants;
 import com.qa.hubspot.utils.ElementUtil;
+
+import io.qameta.allure.Step;
 
 public class LoginPage extends BasePage {
 
@@ -25,15 +26,18 @@ public class LoginPage extends BasePage {
 	}
 
 	// 3. page actions:
+	@Step("get login page title....")
 	public String getLoginPageTitle() {
 		return elementUtil.waitForTitleToBePresent(Constants.LOGIN_PAGE_TITLE, 10);
 	}
 
+	@Step("check sign up link on login page....")
 	public boolean verifySignUpLink() {
 		elementUtil.waitForElementPresent(signUpLink, 5);
 		return elementUtil.doIsDisplayed(signUpLink);
 	}
 
+	@Step("login to app with username: {0} and password: {1}")
 	public HomePage doLogin(String username, String password) {
 		elementUtil.waitForElementPresent(this.username, 10);
 		elementUtil.doSendKeys(this.username, username);
--- src/test/java/com/qa/hubspot/tests/LoginPageTest.java
+++ src/test/java/com/qa/hubspot/tests/LoginPageTest.java
@@ -6,12 +6,23 @@ import org.testng.annotations.Test;
 
 import com.qa.hubspot.base.BaseTest;
 import com.qa.hubspot.listeners.ExtentReportListener;
+import com.qa.hubspot.listeners.TestAllureListener;
 import com.qa.hubspot.utils.Constants;
 
-//@Listeners(ExtentReportListener.class)
-public class LoginPageTest extends BaseTest {
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 
+@Listeners(TestAllureListener.class)
+@Epic("Epic - 101 : design login page with different features...")
+@Story("US - 102 : design basic login page with singup, title and login form...")
+public class LoginPageTest extends BaseTest {
+	
 	@Test(priority = 2)
+	@Description("verify Login Page Title Test.....")
+	@Severity(SeverityLevel.NORMAL)
 	public void verifyLoginPageTitleTest() {
 		String title = loginPage.getLoginPageTitle();
 		System.out.println("login page title is: " + title);
@@ -19,11 +30,15 @@ public class LoginPageTest extends BaseTest {
 	}
 
 	@Test(priority = 1)
+	@Description("verify sugn up link test.....")
+	@Severity(SeverityLevel.CRITICAL)
 	public void verifySignUpLinkTest() {
 		Assert.assertTrue(loginPage.verifySignUpLink(), "sing up link is not displayed....");
 	}
 
 	@Test(priority = 3)
+	@Description("verify Login Test.....")
+	@Severity(SeverityLevel.BLOCKER)
 	public void loginTest() {
 		loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
 	}
