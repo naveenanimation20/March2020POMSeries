4d350c0950ed556d55e700e6403fd3028ab89167
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2020-06-04 09:05:41 +0530

    added tesults listener and screenshot listener

---
+++ src/main/java/com/qa/hubspot/listeners/ScreenshotListener.java
@@ -0,0 +1,36 @@
+package com.qa.hubspot.listeners;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.TestListenerAdapter;
+
+import com.qa.hubspot.base.BasePage;
+
+public class ScreenshotListener extends TestListenerAdapter {
+    @Override
+    public void onTestFailure(ITestResult result) {
+    	BasePage basePage = new BasePage();
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+        String methodName = result.getName();
+        if(!result.isSuccess()){
+            File scrFile = ((TakesScreenshot)basePage.getDriver()).getScreenshotAs(OutputType.FILE);
+            try {
+                String reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/surefire-reports";
+                File destFile = new File((String) reportDirectory+"/failure_screenshots/"+methodName+"_"+formater.format(calendar.getTime())+".png");
+                FileUtils.copyFile(scrFile, destFile);
+                Reporter.log("<a href='"+ destFile.getAbsolutePath() + "'> <img src='"+ destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
